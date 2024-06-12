4621f387769f28459ee180c3adacaad3d32bb09e
Nguyen Lam <nlsc1999@gmail.com>
2024-06-12 12:44:10 +0700

    test

---
+++ server/src/controllers/index.js
@@ -0,0 +1,4 @@
+import Login_registerController from './controller/login_register.js';
+import UserController from './controller/user.js';
+
+export { Login_registerController, UserController };
---
+++ server/src/routers/index.js
@@ -0,0 +1,4 @@
+import Login_RegisterRouter from './router/login_register.js';
+import UserRouter from './router/user.js';
+
+export { Login_RegisterRouter, UserRouter };
