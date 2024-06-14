55fd4d97df0950f87204deb4f88f4a8b1185e03c
Nguyen Lam <nlsc1999@gmail.com>
2024-06-14 12:42:31 +0700

    test

--- server/src/controllers/course.controller.js
+++ server/src/controllers/course.controller.js
@@ -1,4 +1,4 @@
-import Course from '../models/course.model';
+import Course from '../models/course.model.js';
 
 class CourseController {
   // Get all courses
--- server/src/routes/course.route.js
+++ server/src/routes/course.route.js
@@ -5,6 +5,7 @@ import { AuthMiddleware } from '../middlewares/index.js';
 const router = express.Router();
 
 router.get('/get-all', CourseController.index);
+router.post('/add', AuthMiddleware.authAdmin, CourseController.add);
 router.get('/detail/:id', CourseController.get);
 router.put('/update/:id', AuthMiddleware.authAdmin, CourseController.update);
 router.delete('/delete/:id', AuthMiddleware.authAdmin, CourseController.delete);
