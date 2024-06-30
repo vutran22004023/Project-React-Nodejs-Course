84eeb3096dab3f7a9a613aff86e490689d1292b2
Nguyen Lam <nlsc1999@gmail.com>
2024-06-30 12:23:25 +0700

    - user course model

---
+++ server/src/models/user_course.model.js
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+
+const videoStatusSchema = new mongoose.Schema(
+  {
+    videoId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course.videos',
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['not_started', 'in_progress', 'completed'],
+      default: 'not_started',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const chapterStatusSchema = new mongoose.Schema(
+  {
+    chapterId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course.chapters',
+      required: true,
+    },
+    videos: [videoStatusSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userCourseSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    courseId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course',
+      required: true,
+    },
+    chapters: [chapterStatusSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model('UserCourse', userCourseSchema);
