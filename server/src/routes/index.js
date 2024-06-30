d2010cf075dd4ab7c9bf9a3f469910b29256ab50
Nguyen Lam <nlsc1999@gmail.com>
2024-06-30 22:05:08 +0700

    * user-course demo

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,10 +3,14 @@ import Login_RegisterRouter from './login_register.route.js';
 import UserRouter from './user.route.js';
 import CourseRouter from './course.route.js';
 import PayMentRouter from './payment.router.js';
+import UserCourseRouter from './user_course.route.js';
+
 const router = express.Router();
 
 router.use('/', Login_RegisterRouter);
 router.use('/user', UserRouter);
 router.use('/course', CourseRouter);
 router.use('/pay', PayMentRouter);
+router.use('/user-course', UserCourseRouter);
+
 export default router;
